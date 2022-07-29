f03d35f3317369bc887d5ee6c1a2eed1302ecb57
Tom Wilkinson <twilkinson@google.com>
2022-07-29 13:00:27 -0500

    Add a shared_element_transition.d.ts file.

---
+++ shared_element_transition.d.ts
@@ -0,0 +1,12 @@
+interface DocumentTransition {
+    start(callback: () => Promise<void> | void): Promise<void>;
+    abandon(): void;
+  }
+  
+  interface Document {
+    createDocumentTransition(): DocumentTransition;
+  }
+  
+  interface CSSStyleDeclaration {
+    pageTransitionTag: string;
+  }
